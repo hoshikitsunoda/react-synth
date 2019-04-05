57cf1265c10e62d243095ec1c24c71afe39cc2ae
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-04-05 03:34:08 -0700

    created SynthType component #1

---
+++ src/components/SynthType.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class SynthType extends Component {
+  state = { type: '' }
+  render() {
+    return <div>Type</div>
+  }
+}
+
+export default SynthType
