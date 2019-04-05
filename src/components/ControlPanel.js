a8a81ee17c42e11f2a8a4ce88f0a1ee6821ce61b
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-04-05 03:35:13 -0700

    imported SynthType in ControlPanel #1

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Arpeggiator from './Arpeggiator'
 import MonoPoly from './MonoPoly'
+import SynthType from './SynthType'
 
 const ControlPanel = props => {
   return (
     <div>
       <Arpeggiator sendData={props.sendData} sequencer={props.sequencer} />
       <MonoPoly changeVoice={props.changeVoice} />
+      <SynthType />
     </div>
   )
 }
