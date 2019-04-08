0b4976fe0efce38bff1f3cf696e5b0986783d379
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-04-07 22:37:29 -0700

    added onChange function to set input value as octave state #9

--- src/components/ArpeggiatorOctaveMeter.js
+++ src/components/ArpeggiatorOctaveMeter.js
@@ -2,8 +2,25 @@ import React, { Component } from 'react'
 
 class ArpeggiatorOctaveMeter extends Component {
   state = { octave: 3 }
+
+  onChange = event => {
+    this.setState({ octave: parseInt(event.target.value) })
+  }
+
   render() {
-    return <div>AOM</div>
+    return (
+      <div>
+        <input
+          onChange={this.onChange}
+          type="range"
+          value={this.state.octave}
+          name="octave"
+          min="0"
+          max="7"
+        />
+        <div>{this.state.octave}</div>
+      </div>
+    )
   }
 }
 
