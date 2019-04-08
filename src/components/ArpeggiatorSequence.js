79ea74d28fcf263fc19487f36e1b84bfad738fbc
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-04-07 23:08:49 -0700

    created ArpeggiatorSequence component #11

---
+++ src/components/ArpeggiatorSequence.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class ArpeggiatorSequence extends Component {
+  state = {  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default ArpeggiatorSequence;
