0eb2d4bb771856b3e364c5bef440d10eee7211d2
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-04-09 02:48:57 -0700

    added global font and background color #13

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react'
 import Synth from './Synth'
 
+import styled, { createGlobalStyle } from 'styled-components'
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: 'Orbitron', sans-serif;
+  }
+`
+
+const Background = styled.div`
+  display: flex;
+  align-items: center;
+  height: 100vh;
+  background: linear-gradient(
+    to bottom,
+    #959595 0%,
+    #0d0d0d 46%,
+    #010101 50%,
+    #0a0a0a 53%,
+    #4e4e4e 76%,
+    #383838 87%,
+    #1b1b1b 100%
+  );
+`
+
 class App extends Component {
   render() {
     return (
-      <div>
+      <Background>
+        <GlobalStyles />
         <Synth />
-      </div>
+      </Background>
     )
   }
 }
