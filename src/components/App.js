e1728d78996bfcb14843b813584b3f0ab166f307
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-04-09 03:28:44 -0700

    removed background #13

--- src/components/App.js
+++ src/components/App.js
@@ -4,8 +4,17 @@ import Synth from './Synth'
 import styled, { createGlobalStyle } from 'styled-components'
 
 const GlobalStyles = createGlobalStyle`
-  body {
-    font-family: 'Orbitron', sans-serif;
+  body,
+  button,
+  select,
+  select > option {
+    font-family: 'Orbitron', sans-serif !important;
+  }
+
+  button.active,
+  #arp-start.active {
+    background-color: #ffaf37 !important;
+    color: #fff !important;
   }
 `
 
@@ -13,16 +22,6 @@ const Background = styled.div`
   display: flex;
   align-items: center;
   height: 100vh;
-  background: linear-gradient(
-    to bottom,
-    #959595 0%,
-    #0d0d0d 46%,
-    #010101 50%,
-    #0a0a0a 53%,
-    #4e4e4e 76%,
-    #383838 87%,
-    #1b1b1b 100%
-  );
 `
 
 class App extends Component {
