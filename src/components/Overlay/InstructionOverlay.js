08fca9576ed020aa0afa8b5e9f8d03e69e9c8b12
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-04-12 15:27:35 -0700

    moved styled components from overlay to styles.js #18

--- src/components/Overlay/InstructionOverlay.js
+++ src/components/Overlay/InstructionOverlay.js
@@ -1,95 +1,17 @@
 import React from 'react'
 
-import styled from 'styled-components'
-
-const Overlay = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: rgba(0, 0, 0, 0.4);
-  width: 100vw;
-  height: 100vh;
-  position: fixed;
-  z-index: 10;
-  transition: 0.3s;
-  opacity: ${props => (props.isActive ? 1 : 0)};
-  visibility: ${props => (props.isActive ? 'visible' : 'hidden')};
-  transition: opacity 0.3s, visibility 0.3s;
-`
-
-const InstructionBox = styled.div`
-  opacity: ${props => (props.isActive ? 1 : 0)};
-  visibility: ${props => (props.isActive ? 'visible' : 'hidden')};
-  max-width: 50rem;
-  min-width: 50rem;
-  height: 100%;
-  max-height: 29rem;
-  margin: 0 auto;
-  background: linear-gradient(
-    to bottom,
-    rgba(42, 49, 50, 0.7),
-    rgba(42, 49, 50, 0.3)
-  );
-  border-radius: 1rem;
-  transition: opacity 0.3s, visibility 0.3s;
-`
-
-const TextBoxWrap = styled.div`
-  color: #fff;
-  letter-spacing: 0.2rem;
-`
-const NoteLengthTextBox = styled.div`
-  padding: 0.5rem;
-  letter-spacing: 0.2rem;
-  border: 3px solid #ffaf37;
-  border-bottom: none;
-`
-
-const ArpInstructionBox = styled.div`
-  display: flex;
-`
-
-const ArpNotesWrap = styled.div`
-  width: 80%;
-  padding: 0.5rem;
-  border: 3px solid #ffaf37;
-`
-
-const ArpOctaveWrap = styled.div`
-  width: 22%;
-  padding: 0.5rem;
-  border: 3px solid #ffaf37;
-  border-left: none;
-  display: flex;
-  align-items: center;
-`
-
-const ArpButtonWrap = styled.div`
-  width: 15%;
-  padding: 0.5rem;
-  border: 3px solid #ffaf37;
-  border-left: none;
-  display: flex;
-  align-items: center;
-`
-
-const KeyboardSettingWrap = styled.div`
-  display: flex;
-  height: 5rem;
-`
-
-const KeyboardSetting = styled.div`
-  width: 25%;
-  display: flex;
-  align-items: center;
-  padding: 0.5rem;
-  border: 3px solid #ffaf37;
-  border-top: none;
-  border-left: none;
-  &:first-child {
-    border-left: 3px solid #ffaf37;
-  }
-`
+import {
+  Overlay,
+  InstructionBox,
+  TextBoxWrap,
+  NoteLengthTextBox,
+  ArpInstructionBox,
+  ArpNotesWrap,
+  ArpOctaveWrap,
+  ArpButtonWrap,
+  KeyboardSettingWrap,
+  KeyboardSetting
+} from '../styles'
 
 const InstructionOverlay = props => {
   return (
@@ -98,7 +20,7 @@ const InstructionOverlay = props => {
         <TextBoxWrap>
           <NoteLengthTextBox>
             <p>Arpeggiator Note Length:</p>
-            <p>Controls the length of arpeggiator notes being played</p>
+            <p>Controls the length of arpeggiator notes being played.</p>
           </NoteLengthTextBox>
           <ArpInstructionBox>
             <ArpNotesWrap>
