57f14c78c4deec9ecc518b20031348ae96782eaf
Hosh'ki Tsunoda <liquidohr@gmail.com>
2019-04-11 22:09:48 -0700

    created InstructionOverlay component #16

---
+++ src/components/Overlay/InstructionOverlay.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class InstructionOverlay extends Component {
+  state = { isShown: true }
+  render() {
+    return <div>Overlay</div>
+  }
+}
+
+export default InstructionOverlay
